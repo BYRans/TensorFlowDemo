78a929de8af973074aecc7b3071c5492453889a8
Ransford <dingyu.sdu@gmail.com>
2019-08-22 21:52:05 +0800

    modify preprocess_data

--- Keras/test.py
+++ Keras/test.py
@@ -3,11 +3,29 @@ from keras.utils import np_utils
 import numpy as np
 
 np.random.seed(10)
+l=[0 for x in range(0, 10)]
+a=[0 for x in range(0, len(l))]
+print(a)
 
-# Read MNIST data
-(X_Train, y_Train), (X_Test, y_Test) = mnist.load_data()
+x = np.array([[1, 2, 3], [4, 5, 6]])
+y = np.array([0, 1])
+np.savez('F:/数据集/mnist/test.npz', X_train=x, Y_train=y)
+f = np.load('F:/数据集/mnist/test.npz')
+x_train, y_train = f['X_train'], f['Y_train']
+f.close()
+print(x_train)
+print(y_train)
 
 
+
+
+Read MNIST data
+path = 'F:/数据集/mnist/mnist.npz'
+f = np.load(path)
+x_train, y_train = f['x_train'], f['y_train']
+x_test, y_test = f['x_test'], f['y_test']
+f.close()
+print(type(f))
 
 # Translation of data
 X_Train4D = X_Train.reshape(X_Train.shape[0], 28, 28, 1).astype('float32')
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,31 +1,64 @@
 import numpy as np
-
-with open('F:/数据集/Kim2016/malware_dataset/malware_dataset/malware_API_dataset.csv') as raw_data:
-    operations = set()
-    for line in raw_data:
-        operation = line.replace('\n', '').replace('"', '').split(',')
-        del operation[0]
-        del operation[0]
-        operations = operations.union(set(operation))
+import json
 
 
+def create_oprations_set(raw_filename, op_index_filename):
+    with open(raw_filename) as raw_data:
+        operations = set()
+        for line in raw_data:
+            operation = line.replace('\n', '').replace('"', '').split(',')
+            del operation[0]
+            del operation[0]
+            operations = operations.union(set(operation))
+
+    operations_list = list(operations)
+    map_index = range(len(operations))
+    ziped_op_index = zip(operations_list, map_index)
+
+    operations_dic = {k: v for k, v in ziped_op_index}
+
+    with open(op_index_filename, 'w') as json_file:
+        json.dump(operations_dic, json_file, ensure_ascii=False)
+    print("operations index dictionary create success! Dic file saved in ", op_index_filename)
 
 
-filename = 'F:/数据集/Kim2016/malware_dataset/malware_dataset/operations_map.txt'
-with open(filename,'w') as f:
-    i = 0
-    for item in operations:
-        print(item, "  ", i)
-        f.write(item+"  "+str(i)+"\n")
-        i += 1
+def process_raw_data(raw_filename, op_index_filename):
+    np.set_printoptions(threshold=np.inf)
+
+    with open(op_index_filename, 'r') as fileR:
+        operation_dic = json.load(fileR)
+        fileR.close()
+
+    with open(raw_filename) as raw_data:
+        line_num = len(raw_data.readlines())
+
+    with open(raw_filename) as raw_data:
+        processed_data = np.empty(shape=(line_num, len(operation_dic))).astype("int32")
+        labels_list = []
+        i = 0
+        for line in raw_data:
+            tmp_processed_data = [0 for x in range(0, len(operation_dic))]
+            operation = line.replace('\n', '').replace('"', '').split(',')
+            labels_list.append(operation.pop(0))
+            mal_name = operation.pop(0)
+            operation_set = set(operation)
+            for op in operation_set:
+                if len(op) != 0:
+                    index = operation_dic[op]
+                    tmp_processed_data[index] = 1
+            processed_data[i] = np.array(tmp_processed_data)
+            i += 1
+
+        f = open('F:/数据集/Kim2016/malware_dataset/malware_dataset/labels.txt',"w")
+        f.write(str(set(labels_list)))
+
+def main():
+    raw_filename = 'F:/数据集/Kim2016/malware_dataset/malware_dataset/malware_API_dataset.csv'
+    op_index_filename = 'F:/数据集/Kim2016/malware_dataset/malware_dataset/operations_map.json'
+
+    # create_oprations_set(raw_filename, op_index_filename)
+    process_raw_data(raw_filename, op_index_filename)
 
 
-
-# stand_tmp_list = [0]*15
-# stand_list = list(operations)
-# stand_list.extend(stand_tmp_list)
-# operations_np = np.array(stand_list).reshape(22,22,1)
-# np.save('F:/数据集/mnist/tmp',operations_np)
-# f = np.load('F:/数据集/mnist/tmp.npy')
-# print(f)
-# print(type(f))
+if __name__ == "__main__":
+    main()
